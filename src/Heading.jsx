bd402caf7847466359b9b13921343ee060a71ef1
parthu9924 <parthparmar9786@gmail.com>
2021-03-16 11:50:46 +0530

    website

---
+++ src/Heading.jsx
@@ -0,0 +1,7 @@
+const react=require("react");
+
+function Heading(){
+    return <h1>Hello parth parmar pravinbhai</h1>
+}
+
+export default Heading; 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import "./Heading";
+import Heading from "./Heading";
+
+
+
+ReactDom.render(<Heading/>,document.getElementById("root"));
